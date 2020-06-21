3b87a595d8e7c54647686455f569270f032f1deb
Hedi Calabrese <calabrese.hedi@gmail.com>
2020-06-21 01:50:38 -0700

    delete api functionnal

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -26,5 +26,16 @@ router.post("/api/notes", (req, res) => {
 
 // DELETE
 
+router.delete("/api/notes/:id", async (req, res) => {
+    let data = await fs.readFileSync("./db/db.json", "utf8");
+    let notes = JSON.parse(data);
+    const idToDelete = req.params.id;
+    notes.forEach((value, index) => {
+        if (value.id == idToDelete) {
+            notes.splice(index, 1);
+        }
+    });
+    await fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, 2), (err) => { if (err) throw err });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const PORT = 4500;
+const PORT = 4000;
 const path = require("path")
 
 app.use(express.urlencoded({ extended: true }))
